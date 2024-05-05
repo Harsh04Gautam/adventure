9e8f7e5dc28a4dea5138ad0987ca53e19d0546f5
Harsh04Gautam <harsh99gautam@gmail.com>
2024-05-05 17:58:05 -0400

    first commit

---
+++ adventure.py
@@ -0,0 +1,165 @@
+import sys
+import json
+
+
+class Engine:
+    def __init__(self):
+        self.map = json.loads(open(sys.argv[1]).read())
+        self.room_names = set()
+        self.current_room = {}
+        self.inventory = []
+
+        # check map
+        self.check_map_keys()
+        self.check_rooms()
+        self.check_duplicate_names()
+        self.check_exits()
+
+    def start(self):
+        self.change_current_room(self.get_starting_room())
+        while True:
+            try:
+                i = input("What would you like to do? ").replace(" ", "").lower()
+                # quit
+                if i == "quit":
+                    print("Goodbye!")
+                    break
+                self.handle_input(i)
+            except (EOFError):
+                print_err("\nUse 'quit' to exit.")
+
+    # helper methods
+    def check_map_keys(self):
+        missing_keys = []
+        if "start" not in self.map:
+            missing_keys.append("'start'")
+        if "rooms" not in self.map:
+            missing_keys.append("'rooms'")
+        if len(missing_keys) > 0:
+            print_err(f"map is missing key: {', '.join(missing_keys)}", True)
+
+    def check_rooms(self):
+        valid = True
+        for count, room in enumerate(self.map["rooms"]):
+            missing_keys = []
+            if "name" not in room:
+                missing_keys.append("'name'")
+            if "desc" not in room:
+                missing_keys.append("'desc'")
+            if "exits" not in room:
+                missing_keys.append("'exits'")
+
+            if len(missing_keys) > 0:
+                print_err(f"room '{count}' is missing key: {', '.join(missing_keys)}")
+                valid = False
+
+        if not valid:
+            sys.exit(2)
+
+    def check_duplicate_names(self):
+        duplicates = set()
+        for room in self.map["rooms"]:
+            name = room["name"]
+            if name in self.room_names:
+                duplicates.add(name)
+            else:
+                self.room_names.add(name)
+
+        if len(duplicates) > 0:
+            print_err(f"duplicate room name: {', '.join(duplicates)}", True)
+
+    def check_exits(self):
+        invalid_exits = []
+        for room in self.map["rooms"]:
+            for (exit, name) in room["exits"].items():
+                if name not in self.room_names:
+                    invalid_exits.append(room["exits"][exit])
+
+        if len(invalid_exits) > 0:
+            print_err(f"invalid exit: {', '.join(invalid_exits)}", True)
+
+    def get_starting_room(self):
+        current = {}
+        for room in self.map["rooms"]:
+            if room["name"] == self.map["start"]:
+                current = room
+                break
+
+        if current == {}:
+            print_err(f"start room \"{self.map['start']}\" don't exist", True)
+
+        return current
+
+    def change_current_room(self, room):
+        self.current_room = room
+        print(f"> {self.current_room['name']}\n")
+        print(f"{self.current_room['desc']}\n")
+        if "items" in self.current_room and len(self.current_room["items"]) > 0:
+            print(f"Items: {', '.join(self.current_room['items'])}\n")
+        print(f"Exits: {' '.join(self.current_room['exits'].keys())}\n")
+
+    def handle_input(self, input: str) -> None:
+        # go
+        if input.startswith("go"):
+            direction = input[2:]
+
+            if len(direction) == 0:
+                return print_err("Sorry, you need to 'go' somewhere.")
+            if direction not in self.current_room["exits"]:
+                return print_err(f"There's no way to go {direction}.")
+
+            destination = self.current_room["exits"][direction]
+
+            for room in self.map["rooms"]:
+                if room["name"] == destination:
+                    print(f"You go {direction}.\n")
+                    self.change_current_room(room)
+                    return
+        # look
+        if input == "look":
+            return
+
+        # get
+        if input.startswith("get"):
+            item = input[3:]
+            if len(item) == 0:
+                return print_err("Sorry, you need to 'get' something")
+
+            if "items" in self.current_room and item in self.current_room["items"]:
+                self.inventory.append(item)
+                self.current_room["items"].remove(item)
+                print(f"you pick up the {item}.")
+            else:
+                print_err(f"There's no {item} anywhere.")
+            return
+
+        # inventory
+        if input == "inventory":
+            if len(self.inventory) > 0:
+                print("Inventory:")
+                for item in self.inventory:
+                    print(f"\t{item}")
+            else:
+                print_err("You're not carrying anything.")
+            return
+
+
+# helper functions
+def print_err(message: str, exit=False) -> None:
+    print(message, file=sys.stderr)
+    if exit:
+        sys.exit(1)
+
+
+def main():
+    if (len(sys.argv) != 2):
+        print_err("please provide valid arguments", True)
+
+    Engine().start()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt):
+        sys.exit(1)
---
+++ test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+CLEAR="\033[0m"
+
+echo -n "arguments "
+ARGUMET_OUT=$(python adventure.py 2>&1)
+
+if [ "$ARGUMET_OUT" = "please provide valid arguments" ] ; then
+  echo -e "${GREEN} OK ${CLEAR}"
+else
+  echo -e "${RED} FAIL ${CLEAR}"
+fi
+
+echo -n "map validity "
+
+# checking map
+EMPTY_MAP_OUT=$(python adventure.py empty.map 2>&1)
+EMPTY__MAP_ERR="map is missing key: 'start', 'rooms'"
+
+INVALID_ROOM_OUT=$(python adventure.py invalid_room.map 2>&1)
+INVALID_ROOM_ERR="room '1' is missing key: 'exits'
+room '3' is missing key: 'name', 'desc', 'exits'"
+
+DUPLICATE_NAME_OUT=$(python adventure.py duplicate_name.map 2>&1)
+DUPLICATE_NAME_ERR_1="duplicate room name: A white room, A blue room"
+DUPLICATE_NAME_ERR_2="duplicate room name: A blue room, A white room"
+
+INVALID_EXIT_OUT=$(python adventure.py invalid_exit.map 2>&1)
+INVALID_EXIT_ERR="invalid exit: A orange room, A black room"
+
+
+if [ "$EMPTY_MAP_OUT" = "$EMPTY__MAP_ERR" ] &&
+   [ "$INVALID_ROOM_OUT" = "$INVALID_ROOM_ERR" ] &&
+   { [ "$DUPLICATE_NAME_OUT" = "$DUPLICATE_NAME_ERR_1" ] || [ "$DUPLICATE_NAME_OUT" = "$DUPLICATE_NAME_ERR_2" ]; } &&
+   [ "$INVALID_EXIT_OUT" = "$INVALID_EXIT_ERR" ]; then
+    echo -e "${GREEN} OK ${CLEAR}"
+else
+  echo -e "${RED} FAIL ${CLEAR}"
+fi
+
+
+echo -n "start room "
+
+CHECK_START_ROOM=$(python adventure.py invalid_start.map 2>&1)
+CHECK_START_ERR="start room \"A orange room\" don't exist"
+
+if [ "$CHECK_START_ROOM" = "$CHECK_START_ERR" ]; then
+  echo -e "${GREEN} OK ${CLEAR}"
+else
+  echo -e "${RED} FAIL ${CLEAR}"
+fi
